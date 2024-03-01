bd23d5da314e6872dad6089fbdb549033f263007
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-03-01 20:53:47 +0700

    Update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import routerUser from "./RoutesUsers.js";
 import routerToken from "./RoutesToken.js";
-import routerPoint from './RoutesPoint.js'
+import routerPoint from "./RoutesPoint.js";
 function app(express) {
-    express.use("/api/v1/user", routerUser);
-    express.use("/api/v1/token", routerToken);
-    express.use('/api/v1/point', routerPoint)
+  express.use("/api/v1/user", routerUser);
+  express.use("/api/v1/token", routerToken);
+  express.use("/api/v1/point", routerPoint);
 }
 
 export default app;
