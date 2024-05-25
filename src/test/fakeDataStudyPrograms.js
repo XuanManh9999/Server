b54315bd7a0415711feb13bb75407a3bc654bd41
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-05-25 22:16:21 +0700

    Add Fake data import StudyPrograms

--- src/test/fakeDataStudyPrograms.js
+++ src/test/fakeDataStudyPrograms.js
@@ -1,5 +1,96 @@
 // import data
 
 const fake = {
-  
+  nameStudyPrograms: "NGÀNH CÔNG NGHỆ THÔNG TIN",
+  Key: 12,
+  CodeStudyProgram: "7480201",
+  EducationalLevel: "Đại Học",
+  TypeOfEducation: "Chính Quy",
+  Diploma: "Kĩ sư",
+  LanguageOfInstruction: "Tiếng Việt",
+  CompletionTime: "4 năm (đủ 150 tín chỉ)",
+  CreditNumber: "150 (không bao gồm kiến thức GDTC, GDQP&AN)",
+  GradingScale: 4,
+  GraduationRequirements: "Theo quy chế đào tạo của trường)",
+  Extend: "Sau đại học (Thạc sỹ, Tiến sỹ)",
+  Equivalent: [
+    "Chương trình đào tạo ngành CNTT, Đại học Điện Lực; ",
+    "Chương trình đào tạo ngành CNTT, Đại học Đại Nam.",
+  ],
+  JobPosition: [
+    "-	Lập trình viên Phát triển phần mềm ",
+    "-	Kỹ sư phần mềm",
+    "-	Quản trị hệ thống mạng",
+    "-	Quản trị cơ sở dữ liệu",
+    "-	Chuyên viên bảo mật thông tin",
+    "-	Chuyên viên phân tích dữ liệu",
+  ],
+  DataCourse: [
+    {
+      BlockKnowledge: "Kiến thức đại cương",
+      STC: 37,
+      value: [
+        {
+          STT: 1,
+          MHP: "SSH1205",
+          TENHP: "Tư tưởng Hồ Chí Minh",
+          STC: 2,
+          LT_BT: 2,
+          TH: 0,
+          DAMH_BTL: 0,
+          KLTN_DATN_TT: 0,
+          GIO_TH: 30,
+          MHPKQ: "",
+          HK: 4,
+        },
+        {
+          STT: 1,
+          MHP: "SSH1201",
+          TENHP: "Triết học Mác - Lênin",
+          STC: 3,
+          LT_BT: 3,
+          TH: 0,
+          DAMH_BTL: 0,
+          KLTN_DATN_TT: 0,
+          GIO_TH: 45,
+          MHPKQ: "IT1208",
+          HK: 4,
+        },
+      ],
+    },
+    {
+      BlockKnowledge: "Kiến thức cơ sở ngành",
+      STC: 27,
+      value: [
+        {
+          STT: 18,
+          MHP: "IT2201",
+          TENHP: "Cơ sở lập trình với C",
+          STC: 3,
+          LT_BT: 2,
+          TH: 1,
+          DAMH_BTL: 0,
+          KLTN_DATN_TT: 0,
+          GIO_TH: 75,
+          MHPKQ: "",
+          HK: 2,
+        },
+        {
+          STT: 2,
+          MHP: "IT2204",
+          TENHP: "Hệ thống thông tin quản lý",
+          STC: 3,
+          LT_BT: 2,
+          TH: 1,
+          DAMH_BTL: 0,
+          KLTN_DATN_TT: 0,
+          GIO_TH: 75,
+          MHPKQ: "IT1208",
+          HK: 4,
+        },
+      ],
+    },
+  ],
 };
+
+
